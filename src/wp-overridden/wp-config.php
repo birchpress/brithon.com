32eff51475d72098a77c8ef2fb506af1746f614e
Luke Li <luke@birchpress.com>
2015-09-15 22:53:27 -0700

    add theme pagelines

--- src/wp-overridden/wp-config.php
+++ src/wp-overridden/wp-config.php
@@ -29,7 +29,7 @@
         define('DB_PASSWORD', '');
     } else {
         /** Local environment MySQL login info */
-        define('DB_HOST', 'localhost');
+        define('DB_HOST', '127.0.0.1');
         define('DB_USER', 'root');
         define('DB_PASSWORD', '');
     }
